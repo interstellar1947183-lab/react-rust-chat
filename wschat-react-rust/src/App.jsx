64acc79330fd13ca90b217cf55d3f4cb9d4f80f7
Srihari <interstellar1947183@gmail.com>
2026-05-04 03:31:51 +0530

    update websocket url to production

--- wschat-react-rust/src/App.jsx
+++ wschat-react-rust/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     // Initialize WebSocket
     
-   const ws = new WebSocket('wss://your-app-name.onrender.com');
+   const ws = new WebSocket('wss://react-rust-chat-app.onrender.com.com');
     socketRef.current = ws;
 
     ws.onopen = () => console.log("Connected to Rustcord");
